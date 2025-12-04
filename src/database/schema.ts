c7feb35c5af7698bb409062e357242500d17d88e
renan-maestre <renan.maestre.dev@gmail.com>
2025-12-04 00:11:43 -0300

    Update dependencies and add database scripts; include .env in .gitignore

---
+++ src/database/schema.ts
@@ -0,0 +1,15 @@
+import { pgTable, text, uuid } from "drizzle-orm/pg-core";
+
+
+export const users = pgTable('users',{
+    id: uuid("id").primaryKey().defaultRandom(),
+    name: text("name").notNull(),
+    email: text("email").notNull().unique()
+
+})
+
+export const courses =  pgTable('courses',{
+    id: uuid("id").primaryKey().defaultRandom(),
+    title: text().notNull().unique(),
+    description: text()
+})
